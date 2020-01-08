9003b0c68b6bb812876cd4343df12d254210e134
ylafaro0310 <y16mukurou@gmail.com>
2020-01-08 16:53:51 +0900

    First commit

---
+++ public/precache-manifest.006fc704d5454eeb9bc99d2e5024e7dd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0c5ce0ae67da240304fd60171008830",
+    "url": "/index.html"
+  },
+  {
+    "revision": "95a50c3fc83e38169f65",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "57a43c33d1374ba725b8",
+    "url": "/static/js/2.4395d76a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.4395d76a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "95a50c3fc83e38169f65",
+    "url": "/static/js/main.e00fa747.chunk.js"
+  },
+  {
+    "revision": "5d300a419a58d50051e6",
+    "url": "/static/js/runtime-main.947a4f37.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
