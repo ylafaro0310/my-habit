7f4d9b336aae708bd87dde08ec9c8f374b02eb6f
ylafaro0310 <y16mukurou@gmail.com>
2020-02-10 16:28:12 +0900

    Create validation rules

--- app/Models/HabitRecords.php
+++ app/Models/HabitRecords.php
@@ -5,12 +5,29 @@ namespace App\Models;
 use Illuminate\Support\Facades\DB;
 
 class HabitRecords {
+    public $validationRules = [
+        'habit_id' => 'required|integer',
+        'completed_at' => 'required|integer',
+        'is_skipped' => 'required|boolean',
+    ];
+    
     function __construct(){
         $this->pdo = DB::connection()->getPdo();
+        $this->table_name = 'habit_records';
     }
+
     public function get(){
-        $pdo->prepare('select * from habit_records');
-        $pdo->execute();
+        $this->pdo->prepare("select * from ${$this->table_name}");
+        $this->pdo->execute();
         return $pdo->fetchAll();
+    }
+
+    public function insert($params){
+        $this->pdo->prepare("insert into ${$this->table_name}");
+    }
+
+    public function delete($id){
+        $this->pdo->prepare("delete from ${$this->table_name} where id = ${id}");
+        $this->pdo->execute();
     }
 }
--- app/Models/Habits.php
+++ app/Models/Habits.php
@@ -3,11 +3,32 @@
 namespace App\Models;
 
 use Illuminate\Support\Facades\DB;
+use Illuminate\Validation\Rule;
 
 class Habits {
+    public $validationRules = [
+        'habit_name' => 'required|string|max255',
+        'repeat_type' => [
+            'required',
+            'string',
+            Rule::in(['day','week','day_of_week']),
+        ],
+        'repeat_value' => 'required|integer',
+        'started_at' => 'required|integer',
+        'target_time' => 'nullable|integer',
+        'time_of_day' => [
+            'required',
+            'string',
+            Rule::in(['always','am','pm']),
+        ],
+        'consecutive_days' => 'required|integer',
+        'consecutive_weeks' => 'nullable|integer',
+    ];
+
     function __construct(){
         $this->pdo = DB::connection()->getPdo();
     }
+    
     public function get(){
         $pdo->prepare('select * from habits');
         $pdo->execute();
