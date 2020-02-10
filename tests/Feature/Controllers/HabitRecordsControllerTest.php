ec895b96d832f75e392396fd33fc899f3c959821
ylafaro0310 <y16mukurou@gmail.com>
2020-02-10 16:28:28 +0900

    Create HabitRecordsController test

--- tests/Feature/Controllers/HabitRecordsControllerTest.php
+++ tests/Feature/Controllers/HabitRecordsControllerTest.php
@@ -11,7 +11,9 @@ class HabitRecordsControllerTest extends TestCase
 {
     protected function setUp(): void{
         parent::setUp();
+        $habitRecords = new HabitRecords();
         $this->habitRecordsPath = '/habits/records';
+        $this->tableName = $habitRecords->table_name;
     }
 
     /**
@@ -22,7 +24,7 @@ class HabitRecordsControllerTest extends TestCase
     public function testIndex()
     {
         $response = $this->get($this->habitRecordPath);
-        
+        $this->assertArrayHasKey('habit_name',$response);
     }
 
     /**
@@ -32,5 +34,27 @@ class HabitRecordsControllerTest extends TestCase
      */
     public function testStore()
     {
+        $params = [
+            'habit_id' => 3,
+            'completed_at' => Date('2020-02-10'),
+            'is_skipped' => false,
+        ];
+        $this->post($this->habitRecordsPath,$params);
+        $this->assertDatabaseHas($this->tableName, $params);
+
+        // 存在しないhabit_idのテスト
+    }
+
+    /**
+     * delete() test
+     *
+     * @return void
+     */
+    public function testDelete()
+    {
+        $this->delete($this->habitRecordsPath.'/1');
+        $this->assertDatabaseMissing($this->tableName, ['id'=>1]);
+
+        // 存在しないhabit_idのテスト
     }
 }
