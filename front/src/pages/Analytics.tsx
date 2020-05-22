66ece3a2978f65b50abd27ef93039b6559e228a3
ylafaro0310 <y16mukurou@gmail.com>
2020-05-22 17:14:10 +0900

    Modify Analytics

--- front/src/pages/Analytics.tsx
+++ front/src/pages/Analytics.tsx
@@ -1,9 +1,30 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import dayjs from '../lib/dayjs-ja';
 import StyledCard from '../components/Card';
 
-class Analytics extends React.Component {
+type AnalyticsState = {
+  month: number;
+};
+class Analytics extends React.Component<{}, AnalyticsState> {
+  constructor(props: {}) {
+    super(props);
+    this.state = {
+      month: Number(dayjs().format('M')),
+    };
+    this.onChangeMongh = this.onChangeMongh.bind(this);
+  }
+
+  onChangeMongh(add = true): void {
+    const { month } = this.state;
+    if (add) {
+      this.setState({ month: month + 1 });
+    } else {
+      this.setState({ month: month - 1 });
+    }
+  }
+
   calendar(month: number) {
     const dateMonth = dayjs().set('month', month);
     const startOfDay = dateMonth.startOf('month');
@@ -15,39 +36,83 @@ class Analytics extends React.Component {
       tr = [];
       for (let j = 0; j < 7; j++) {
         if ((i === 0 && j < startOfDay.day()) || date.isAfter(endOfDay)) {
-          tr.push(<td></td>);
+          tr.push(<td align='center' key={j}></td>);
           continue;
         }
-        tr.push(<td>{date.format('DD')}</td>);
+        tr.push(
+          <td align='center' key={j}>
+            {date.format('D')}
+          </td>,
+        );
         date = date.add(1, 'd');
       }
-      tbody.push(<tr>{tr}</tr>);
+      tbody.push(<tr key={i}>{tr}</tr>);
     }
     return (
       <>
-        <div>
-          {dayjs()
-            .set('month', month)
-            .format('YYYY年MM月')}
-        </div>
-        <table>
+        <Header>
+          <div
+            onClick={() => {
+              this.onChangeMongh(false);
+            }}
+          >
+            <span className='fas fa-angle-left' />
+          </div>
+          <div>
+            {dayjs()
+              .set('month', month)
+              .format('YYYY年MM月')}
+          </div>
+          <div
+            onClick={() => {
+              this.onChangeMongh(true);
+            }}
+          >
+            <span className='fas fa-angle-right' />
+          </div>
+        </Header>
+        <Table>
           <thead>
-            <th>日</th>
-            <th>月</th>
-            <th>火</th>
-            <th>水</th>
-            <th>木</th>
-            <th>金</th>
-            <th>土</th>
+            <tr>
+              <th>日</th>
+              <th>月</th>
+              <th>火</th>
+              <th>水</th>
+              <th>木</th>
+              <th>金</th>
+              <th>土</th>
+            </tr>
           </thead>
           <tbody>{tbody}</tbody>
-        </table>
+        </Table>
       </>
     );
   }
   render() {
-    return <StyledCard>{this.calendar(dayjs().get('month'))}</StyledCard>;
+    return <StyledCard>{this.calendar(this.state.month - 1)}</StyledCard>;
   }
 }
 
 export default Analytics;
+
+const Header = styled.div`
+  display: flex;
+  justify-content: space-between;
+
+  &:nth-child(1),
+  :nth-child(3) {
+    cursor: pointer;
+  }
+`;
+
+const Table = styled.table`
+  width: 70%;
+  margin: 0 auto;
+  & td {
+    width: 2rem;
+    height: 2rem;
+    background: #55f;
+    border-radius: 0%;
+    content: '';
+  }
+`;
